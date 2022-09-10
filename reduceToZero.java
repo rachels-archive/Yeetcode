10cd7e5071ba4ea33d687892d6a54f1d8359bc3c
rachels-archive <79963756+rachels-archive@users.noreply.github.com>
2022-09-10 17:30:14 +0800

    Create reduceToZero.java

---
+++ reduceToZero.java
@@ -0,0 +1,18 @@
+// Problem 1342
+class Solution {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        
+        while (num != 0) {
+            if (num % 2 == 0){
+                num = num / 2;
+                count += 1;
+            } else {
+                num -= 1;
+                count += 1;
+            }
+        }
+        return count;
+        
+    }
+}
